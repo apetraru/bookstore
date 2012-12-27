a325ba0b49ccdecd79b81d02a76c83ab3367e144
Andrei Petraru <petraru.andrei@yahoo.com>
2012-12-27 19:23:08 +0200

    Added querydsl dependencies

--- src/main/java/com/bk/bean/CustomerBean.java
+++ src/main/java/com/bk/bean/CustomerBean.java
@@ -5,48 +5,54 @@ import com.bk.model.Customer;
 import com.bk.model.EmailAddress;
 import com.bk.repository.CustomerRepository;
 import javax.annotation.PostConstruct;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 /**
- * @author ph
- * Date: 12/26/12
+ * @author ph Date: 12/26/12
  */
-
 @Component
 @Scope("session")
 public class CustomerBean {
-    @Autowired
-    CustomerRepository repository;
 
-    @PersistenceContext
-    EntityManager em;
+	@Autowired
+	CustomerRepository repository;
 
-    private Customer customer;
+	private Customer addedCustomer;
+	private Customer foundCustomer;
 
-    @PostConstruct
-    public void init() {
-        customer = new Customer("alex", "ionel");
-    }
+	@PostConstruct
+	public void init() {
+		addedCustomer = new Customer("c1", "c1");
+		foundCustomer = new Customer("c2", "c2");
+	}
 
-    public Customer getCustomer() {
-        return customer;
-    }
+	public Customer getAddedCustomer() {
+		return addedCustomer;
+	}
 
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
-    }
+	public void setAddedCustomer(Customer addedCustomer) {
+		this.addedCustomer = addedCustomer;
+	}
 
-    public void findCustomer() {
-        EmailAddress emailAddress = new EmailAddress("andrei@yahoo.com");
-        Address address = new Address("ciornei", "iasi", "romania");
-        customer.setEmailAddress(emailAddress);
-        customer.add(address);
-		customer = repository.save(customer);
+	public Customer getFoundCustomer() {
+		return foundCustomer;
+	}
 
-    }
+	public void setFoundCustomer(Customer foundCustomer) {
+		this.foundCustomer = foundCustomer;
+	}
 
+	public void addCustomer() {
+		EmailAddress emailAddress = new EmailAddress("andrei@yahoo.com");
+		Address address = new Address("asd", "asd", "asd");
+		addedCustomer.setEmailAddress(emailAddress);
+		addedCustomer.add(address);
+		addedCustomer = repository.save(addedCustomer);
+	}
+
+	public void findCustomer(Long id) {
+		foundCustomer = repository.findById(id);
+	}
 }
