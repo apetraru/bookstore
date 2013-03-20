276350420b05cfa9b826dbaa3b2bbd1c6586d0a1
Andrei Petraru <phasola@gmail.com>
2013-03-20 22:06:14 +0200

    Removed unused methods from book repository

--- src/main/java/com/bk/repository/BookRepository.java
+++ src/main/java/com/bk/repository/BookRepository.java
@@ -1,28 +1,25 @@
 package com.bk.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.bk.model.Book;
 
 /**
- * User: ph
- * Date: 1/28/13
+ * User: Andrei Petraru
+ * Date: 28 Jan 2013
  */
 
 @Transactional(readOnly = true)
-public interface BookRepository extends Repository<Book, Long>{
+public interface BookRepository extends Repository<Book, Long> {
 
-    Book findByIsbn(String isbn);
+	Book findByIsbn(String isbn);
 
-    @Transactional
-    Book save(Book book);
+	@Transactional
+	Book save(Book book);
 
-    Book findById(Long id);
-    
-    Long count();
-    
-    Page<Book> findAll(Pageable pageable);
+	Book findById(Long id);
+
+	Long count();
+
 }
