398b4ec9c9fd83f46db66ec4ea2fd78851da5bfb
Andrei Petraru <phasola@gmail.com>
2013-04-28 19:14:27 +0300

    AuthorRepository integration tests

---
+++ src/test/java/com/bk/repository/AuthorRepositoryTest.java
@@ -0,0 +1,53 @@
+package com.bk.repository;
+
+import com.bk.common.AbstractIntegrationTest;
+import com.bk.model.Author;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author: Andrei Petraru
+ * Date: 4/27/13
+ */
+public class AuthorRepositoryTest extends AbstractIntegrationTest {
+
+    @Autowired
+    private AuthorRepository repository;
+
+    private static final String AUTHOR_NAME = "VASILE";
+    private static final Long ID = 1L;
+
+    @Before
+    public void setUp() {
+        Author author = new Author();
+        author.setName(AUTHOR_NAME);
+        repository.save(author);
+    }
+
+    @Test
+    public void findByNameTest() {
+        Author author = repository.findByName(AUTHOR_NAME);
+        assertEquals(author.getName(), AUTHOR_NAME);
+    }
+
+    @Test
+    @Ignore
+    public void findByAuthorId() {
+        // TODO
+        // Fix findById() + save() issue
+        Author author = repository.findById(ID);
+        assertEquals(author.getId(), ID);
+    }
+
+    @Test
+    public void saveTest() {
+        Author newAuthor = new Author();
+        newAuthor = repository.save(newAuthor);
+        assertEquals(newAuthor.getId(), (Long) 2L);
+    }
+
+}
