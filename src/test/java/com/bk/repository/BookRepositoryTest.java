c484d0d1609ca5feead7ba371d2a1fa7504a35fe
Andrei Petraru <phasola@gmail.com>
2013-04-28 19:14:35 +0300

    BookRepository integration tests

---
+++ src/test/java/com/bk/repository/BookRepositoryTest.java
@@ -0,0 +1,73 @@
+package com.bk.repository;
+
+import com.bk.common.AbstractIntegrationTest;
+import com.bk.model.Book;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @author: Andrei Petraru
+ * Date: 4/27/13
+ */
+public class BookRepositoryTest extends AbstractIntegrationTest {
+    @Autowired
+    private BookRepository repository;
+
+    private static final String ISBN = "9780439064866";
+    private static final Long ID = 1L;
+    private static final Long count = 1L;
+
+    @Before
+    public void setUp() {
+        Book book = new Book();
+        book.setIsbn(ISBN);
+        book.setTitle("title");
+        book.setPages(25);
+        repository.save(book);
+    }
+
+    @Test
+    @Ignore
+    public void findByIdTest() {
+        // TODO
+        // Fix findById() + save() issue
+        Book book = repository.findById(1L);
+        assertNotNull(book);
+    }
+
+    @Test
+    public void findByIsbnTest() {
+        Book book = repository.findByIsbn(ISBN);
+        assertNotNull(book);
+    }
+
+    @Test
+    public void saveTestOk() {
+        Book book = new Book();
+        book.setTitle("title");
+        book.setIsbn("isbn");
+        book.setPages(50);
+        book = repository.save(book);
+        assertNotNull(book);
+    }
+
+    @Test(expected = org.springframework.dao.DataIntegrityViolationException.class)
+    public void duplicateIsbnSaveTestFail() {
+        Book book = new Book();
+        book.setTitle("title");
+        book.setIsbn(ISBN);
+        book.setPages(50);
+        book = repository.save(book);
+    }
+
+    @Test
+    public void countTest() {
+        Long bookCount = repository.count();
+        assertEquals(bookCount, count);
+    }
+}
