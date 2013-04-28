a78b1cf0461d8d3e7f408688b2490ced6e74ec36
Andrei Petraru <phasola@gmail.com>
2013-04-28 19:14:41 +0300

    CustomerRepository integration tests

---
+++ src/test/java/com/bk/repository/CustomerRepositoryTest.java
@@ -0,0 +1,89 @@
+package com.bk.repository;
+
+import com.bk.common.AbstractIntegrationTest;
+import com.bk.model.Customer;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author: Andrei Petraru
+ * Date: 4/28/13
+ */
+public class CustomerRepositoryTest extends AbstractIntegrationTest {
+    private static final String USERNAME = "user";
+    private static final String NEW_USERNAME = "user2";
+    private static final String PASSWORD = "password";
+    private static final String EMAIL = "email@email.com";
+    private static final String NEW_EMAIL = "newemail@email.com";
+    private static final Long ID = 1L;
+
+    @Autowired
+    private CustomerRepository repository;
+
+    @Before
+    public void setUp() {
+        Customer customer = new Customer();
+        customer.setUsername(USERNAME);
+        customer.setPassword(PASSWORD);
+        customer.setEmailAddress(EMAIL);
+        repository.save(customer);
+    }
+
+    @Test
+    @Ignore
+    public void findByIdTest() {
+        Customer customer = repository.findById(ID);
+        assertEquals(customer.getId(), ID);
+    }
+
+    @Test
+    public void findByEmailAddressTest() {
+        Customer customer = repository.findByEmailAddress(EMAIL);
+        assertNotNull(customer);
+    }
+
+    @Test
+    public void findByUsernameTest() {
+        Customer customer = repository.findByUsername(USERNAME);
+        assertNotNull(customer);
+    }
+
+    @Test
+    public void saveTestOk() {
+        Customer customer = new Customer();
+        customer.setUsername(NEW_USERNAME);
+        customer.setPassword(PASSWORD);
+        customer.setEmailAddress(NEW_EMAIL);
+        customer = repository.save(customer);
+        assertNotNull(customer);
+    }
+
+    @Test(expected = org.springframework.dao.DataIntegrityViolationException.class)
+    public void duplicateUserSaveTestFail() {
+        Customer customer = new Customer();
+        customer.setUsername(USERNAME);
+        customer.setPassword(PASSWORD);
+        customer.setEmailAddress(NEW_EMAIL);
+        customer = repository.save(customer);
+    }
+
+    @Test(expected = org.springframework.dao.DataIntegrityViolationException.class)
+    public void duplicateEmailSaveTestFail() {
+        Customer customer = new Customer();
+        customer.setUsername(NEW_USERNAME);
+        customer.setPassword(PASSWORD);
+        customer.setEmailAddress(EMAIL);
+        customer = repository.save(customer);
+    }
+
+    @Test(expected = org.springframework.dao.DataIntegrityViolationException.class)
+    public void emptyUserSaveTestFail() {
+        Customer customer = new Customer();
+        repository.save(customer);
+    }
+
+}
