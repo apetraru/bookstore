4627b6ca8538f1121d1b8e29290e4dc985722290
Andrei Petraru <phasola@gmail.com>
2013-04-29 11:14:22 +0300

    Added reset for table id method

--- src/test/java/com/bk/common/AbstractIntegrationTest.java
+++ src/test/java/com/bk/common/AbstractIntegrationTest.java
@@ -1,5 +1,8 @@
 package com.bk.common;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -16,5 +19,12 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @TransactionConfiguration(defaultRollback = true)
 public abstract class AbstractIntegrationTest {
+	@PersistenceContext
+	private EntityManager entityManager;
 
+	// If RESTART IDENTITY is specified, all table IDENTITY sequences and
+	// all SEQUENCE objects in the schema are reset to their start values
+	protected void reset() {
+		entityManager.createNativeQuery("TRUNCATE SCHEMA PUBLIC RESTART IDENTITY AND COMMIT NO CHECK").executeUpdate();
+	}
 }
