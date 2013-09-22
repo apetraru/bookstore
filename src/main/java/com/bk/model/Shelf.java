587298165d45ffebb688761c4593425cf465ce28
Andrei Petraru <phasola@gmail.com>
2013-09-22 17:27:40 +0300

    Added shelf entity

--- src/main/java/com/bk/model/Shelf.java
+++ src/main/java/com/bk/model/Shelf.java
@@ -1,20 +1,31 @@
 package com.bk.model;
 
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import com.bk.enums.Status;
 
 /**
- * @author Andrei Petraru 
+ * @author Andrei Petraru
  * 9 Jul 2013
  */
+
+@Entity
+@Table(name = "SHELF")
 public class Shelf extends AbstractEntity {
 	private Status status;
+
+	@OneToMany
 	private Set<Book> shelf = new HashSet<>();
+	private Date startDate;
+	private Date endDate;
 
 	@ManyToOne
 	private Book book;
@@ -52,5 +63,21 @@ public class Shelf extends AbstractEntity {
 
 	public Set<Book> getShelf() {
 		return Collections.unmodifiableSet(shelf);
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
 	}
 }
