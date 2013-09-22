3c6a4b3b791f85c32564dc7de51901cad001998e
Andrei Petraru <phasola@gmail.com>
2013-09-22 17:27:54 +0300

    Updated shelf repository

--- src/main/java/com/bk/repository/ShelfRepository.java
+++ src/main/java/com/bk/repository/ShelfRepository.java
@@ -1,16 +1,14 @@
 package com.bk.repository;
 
-import java.io.Serializable;
-
+import com.bk.model.Book;
+import com.bk.model.Customer;
+import com.bk.model.Shelf;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.bk.model.Book;
-import com.bk.model.Customer;
-import com.bk.model.Review;
-import com.bk.model.Shelf;
+import java.io.Serializable;
 
 /**
  * @author Andrei Petraru
@@ -22,6 +20,6 @@ public interface ShelfRepository extends Repository<Shelf, Long>, Serializable{
 	@Transactional
 	Shelf save(Shelf shelf);
 	
-	@Query("select s from Shelf r where r.book = :book and r.customer = :customer")
-    Review getCustomerShelf(@Param("book")Book book, @Param("customer") Customer customer);
+	@Query("select s from Shelf s where s.book = :book and s.customer = :customer")
+    Shelf getCustomerShelf(@Param("book")Book book, @Param("customer") Customer customer);
 }
