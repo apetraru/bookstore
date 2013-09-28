d26876d023ee1b5e88beb83322bf7920ff10f26b
Andrei Petraru <phasola@gmail.com>
2013-09-28 16:20:19 +0300

    More button will be hidden for author about < 500 chars

--- src/main/webapp/resources/js/show-hide.js
+++ src/main/webapp/resources/js/show-hide.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+
+    var $aboutLength = $('.author-about').text().length;
+    if ($aboutLength < 500) {
+        $('.author-show-more').css('display', 'none');
+    }
+
 	$('.author-show-more').on('click', function(e) {
 		$('.author-about').toggleClass('author-about-expanded');
 	});
