21102523c33c40cc52c452b422266ea2d17ed894
Andrei Petraru <phasola@gmail.com>
2014-08-21 15:05:46 +0300

    Temporary fix for crashing on home page

--- src/main/java/com/bk/bean/HomeBean.java
+++ src/main/java/com/bk/bean/HomeBean.java
@@ -1,14 +1,19 @@
 package com.bk.bean;
 
-import com.bk.lazydatamodel.GenreLazyDataModel;
-import com.bk.model.Book;
-import com.bk.service.BookService;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import java.util.*;
+import com.bk.lazydatamodel.GenreLazyDataModel;
+import com.bk.model.Book;
+import com.bk.service.BookService;
 
 /**
  * @author Andrei Petraru
@@ -21,42 +26,33 @@ public class HomeBean {
 
 	private static final int DISPLAYED_BOOKS = 15;
 
-	@Autowired
-	private BookService bookService;
+	@Autowired private BookService bookService;
+	@Autowired private GenreLazyDataModel lazyModel;
+	@Autowired private RatingsBean ratings;
 
-	@Autowired
-	private GenreLazyDataModel lazyModel;
-	
-	@Autowired
-	private RatingsBean ratings;
-
-	private List<Book> books = new ArrayList<>();
+	private Set<Book> books = new HashSet<>();
 
 	@PostConstruct
 	public void init() {
-		Set<Book> noDuplicates = new HashSet<>();
-		Random random = new Random();
-		int count = bookService.count().intValue();
-
-		if (count < DISPLAYED_BOOKS) {
-			return;
-		}
-
-		for (int i = 0; i < DISPLAYED_BOOKS; i++) {
-			Book book = bookService.findById((long) random.nextInt(count));
-			noDuplicates.add(book);
-		}
-		books.addAll(noDuplicates);
+//		Random random = new Random();
+//		int count = bookService.count().intValue();
+//
+//		if (count < DISPLAYED_BOOKS) {
+//			return;
+//		}
+//		while (books.size() < DISPLAYED_BOOKS) {
+//			books.add(bookService.findById((long) random.nextInt(count)));
+//		}
 	}
 
-	public List<Book> getBooks() {
-		return Collections.unmodifiableList(books);
+	public Set<Book> getBooks() {
+		return Collections.unmodifiableSet(books);
 	}
 
 	public GenreLazyDataModel getLazyModel() {
 		return lazyModel;
 	}
-	
+
 	public RatingsBean getRatings() {
 		return ratings;
 	}
