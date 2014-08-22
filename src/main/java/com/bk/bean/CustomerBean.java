abc6ed482e0b6dd36dbc9f53ba3f7f8ffbff3458
Andrei Petraru <phasola@gmail.com>
2014-08-22 13:44:46 +0300

    Added customer profile page

---
+++ src/main/java/com/bk/bean/CustomerBean.java
@@ -0,0 +1,41 @@
+package com.bk.bean;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.bk.model.Customer;
+import com.bk.service.CustomerService;
+
+@Component
+@Scope("view")
+public class CustomerBean {
+	@Autowired private CustomerService customerService;
+	@Autowired private SessionBean sessionBean;
+
+	private Customer customer;
+	
+	@PostConstruct
+	public void init() {
+		customer = sessionBean.getLoggedInUser();
+		if (customer == null) {
+			String message = "Please login first to view your profile";
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, message, null));
+			return;
+		}
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+}
