10a34dc0c275f0754dfa9cf3f51b114eb9bf0e20
Andrei Petraru <phasola@gmail.com>
2014-09-05 15:46:14 +0300

    Added message bundle retrieval

--- src/main/java/com/bk/util/Message.java
+++ src/main/java/com/bk/util/Message.java
@@ -1,5 +1,7 @@
 package com.bk.util;
 
+import java.util.ResourceBundle;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
@@ -14,5 +16,17 @@ public final class Message {
     }
 
     private Message() {
+    }
+    
+    public static void error(String id, String messageBody) {
+    	addMessage(id, messageBody, FacesMessage.SEVERITY_ERROR);
+    }
+    
+    public static void info(String id, String messageBody) {
+    	addMessage(id, messageBody, FacesMessage.SEVERITY_INFO);
+    }
+    
+    public static String msg(String key) {
+    	return ResourceBundle.getBundle("messages").getString(key);
     }
 }
