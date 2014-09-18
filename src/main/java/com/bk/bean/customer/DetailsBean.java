991dfb29e6fa8faa06f40001df29600e3d31aada
Andrei Petraru <phasola@gmail.com>
2014-09-18 15:56:21 +0300

    Customer can upload profile picture

--- src/main/java/com/bk/bean/customer/DetailsBean.java
+++ src/main/java/com/bk/bean/customer/DetailsBean.java
@@ -13,30 +13,31 @@ import org.springframework.stereotype.Component;
 
 import com.bk.bean.SessionBean;
 import com.bk.model.Customer;
+import com.bk.model.Image;
 import com.bk.service.CustomerService;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
 
 @Component
-@Scope("view")
+@Scope("session")
 public class DetailsBean {
+
 	@Autowired private CustomerService customerService;
 	@Autowired private SessionBean sessionBean;
 
 	private Customer customer;
+	private Image image;
 
 	@PostConstruct
 	public void init() {
 		customer = sessionBean.getLoggedInUser();
+		image = new Image();
 		if (customer == null) {
 			globalError(msg("loginFirst"));
 		}
-	}
-
-	public Customer getCustomer() {
-		return customer;
-	}
-
-	public void setCustomer(Customer customer) {
-		this.customer = customer;
 	}
 
 	public void update() {
@@ -49,9 +50,29 @@ public class DetailsBean {
 			error("table:profileForm:email", msg("emailTaken"));
 			return;
 		}
-
-		customer = customerService.save(customer);
+		sessionBean.setLoggedInUser(customerService.save(customer));
+		image = new Image();
 		info("table:profileForm:updateButton", msg("detailsUpdated"));
+	}
+
+	public void upload(FileUploadEvent event) throws IOException {
+		UploadedFile uploadedFile = event.getFile();
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		InputStream stream = uploadedFile.getInputstream();
+		byte[] buffer = new byte[(int) uploadedFile.getSize()];
+		int read;
+		try {
+			while ((read = stream.read(buffer)) != -1) {
+				baos.write(buffer, 0, read);
+			}
+		} finally {
+			stream.close();
+		}
+		image.setName(uploadedFile.getFileName());
+		image.setSize(uploadedFile.getSize());
+		image.setFileContent(baos.toByteArray());
+		image.setContentType(uploadedFile.getContentType());
+		customer.setImage(image);
 	}
 
 	private boolean usernameExists() {
@@ -60,7 +81,7 @@ public class DetailsBean {
 		if (duplicateCustomer == null) {
 			return false;
 		}
-		
+
 		return (customer.getUsername().equals(duplicateCustomer.getUsername()) && !duplicateCustomer
 				.getId().equals(customer.getId()));
 	}
@@ -71,9 +92,17 @@ public class DetailsBean {
 		if (duplicateCustomer == null) {
 			return false;
 		}
-		
+
 		return (customer.getEmailAddress().equals(duplicateCustomer.getEmailAddress()) && !duplicateCustomer
 				.getId().equals(customer.getId()));
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
 	}
 
 }
