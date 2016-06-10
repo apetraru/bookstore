413477bedbba2c9e83a4c0984405a408d9bcf654
Andrei Petraru <phasola@gmail.com>
2016-06-10 22:06:20 +0300

    Added genres on home page

--- src/main/java/com/bk/bean/HomeBean.java
+++ src/main/java/com/bk/bean/HomeBean.java
@@ -1,33 +1,49 @@
 package com.bk.bean;
 
-import com.bk.model.Book;
-import com.bk.service.BookService;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.List;
+import com.bk.lazydatamodel.GenreLazyDataModel;
+import com.bk.model.Book;
+import com.bk.service.BookService;
 
 /**
- * @author Andrei Petraru
+ * @author Andrei Petraru 
  * 15 Mar 2013
  */
 
 @Component
 @Scope("request")
 public class HomeBean {
-    @Autowired private BookService bookService;
+	@Autowired
+	private BookService bookService;
+	@Autowired
+	private GenreLazyDataModel lazyModel;
+	@Autowired
+	private RatingsBean ratings;
 
-    private List<Book> books = new ArrayList<>();
+	private List<Book> books;
 
-    @PostConstruct
-    public void init() {
-        books = bookService.findRandom();
-    }
+	@PostConstruct
+	public void init() {
+		books = bookService.findRandom();
+	}
 
-    public List<Book> getBooks() {
-        return books;
-    }
+	public List<Book> getBooks() {
+		return books;
+	}
+
+	public GenreLazyDataModel getLazyModel() {
+		return lazyModel;
+	}
+
+	public RatingsBean getRatings() {
+		return ratings;
+	}
+
 }
